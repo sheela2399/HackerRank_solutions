7cf4b981a5bbf6cb6496e4e64046c72825a6188e
sheela mishra <sheelamishra1999@gmail.com>
2024-05-22 23:08:37 +0530

    Day2

--- solutions.js
+++ solutions.js
@@ -7,14 +7,6 @@ for (let i = 0; i <= 10; i++) {
 }
 
 // 3. Print Even number from 0-10
-function evenNum(i) {
-    for (i = 0; i <= 10; i++) {
-        if (i / 2 == 0) {
-            console.log("it's a even number")
-        } else { "it's a odd number" }
-    }
-} evenNum(4)
-
 function printEvenNum() {
     for (i = 0; i <= 10; i += 2) {
         console.log(i)
@@ -30,7 +22,7 @@ function square() {
 } square()
 
 //5.Print a table containing multiplication tables
-let n = prompt(parseInt('Enter a number'))
+ let n = prompt(parseInt('Enter a number'))
 for (i = 1; i <= 10; i++) {
     let result = i * n;
     console.log(`${n} x ${i} = ${result}`)
@@ -38,20 +30,98 @@ for (i = 1; i <= 10; i++) {
 
 //6.Create a length converter function
 function lengthConverter() {
-    let k = prompt(parseInt('Enter length in km'))
+     let k = prompt(parseInt('Enter length in km'))
     kmToMiles = k * 0.621371;
     console.log(`${k} km = ${kmToMiles} miles`)
 } lengthConverter()
 
 // 7.Calculate the sum of numbers within an array
+    const arr = [2, 4, 5, 7, 6];
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];}
+        console.log(sum);
+    
 
-function sum() {
-    let a = [2, 4, -5, -7, 6];
-     let sum = 0;
-     for(i =0; i <= a.length; i++){
-        sum = sum + a[i];
-        console.log(sum)
-     }
-}sum()
+//  8. Create a function that reverses an array
+function arrReverse(){
+    let originalArray =[2,3,4,5,6];
+    console.log(originalArray);
+   let arrReverse = originalArray.reverse();
+    console.log(arrReverse);
+}
+arrReverse()
 
+// by using loop    
+let Array1 = [2,3,4,5,6];
+let Array2 =[];
+console.log(Array1);
+for( let i= Array1.length - 1; i >= 0; i --){
+    Array2.push(Array1[i])
+}
+console.log(Array2)
+
+//9. Sort an array from lowest to highest
+var ar = [9,6,4,8,3,2,7];
+var ar2 = ar.sort();
+console.log(ar2);
+
+// or
+// ascending order
+var  ar = [9,6,4,8,3,2,7];
+ar.sort((a,b) => (a - b));
+console.log(ar)
+
+// Descending order
+var  ar = [9,6,4,8,3,2,7];
+ar.sort((a,b) => (b - a));
+console.log(ar)
+
+// 10. Create a function that filters out negative numbers
+function filterNeg(){
+     var ar = [1, 6,-2,-4];
+  var ar2 = ar.filter(number => number > 0);
+  console.log(ar2)
+}filterNeg()
+
+// 11.Remove the spaces found in a string
+let oldString = "   hello     shaily  "
+var newstring = oldString.replace (/ /g, "");
+console.log(newstring)
+
+// or to remove spaces from both side
+console.log(oldString.trim())
+
+// // 12.Return a Boolean if a number is divisible by 10
+function divisibleBy10(){
+    let i = prompt("Enter a number")
+    if(i % 10 === 0){
+       console.log(true);
+    }else{
+        console.log(false);
+    }    
+} divisibleBy10()
+
+// 13.Return the number of vowels in a string
+
+ function countVowels(str){
+    const vowels = ["a","e","i","o","u","A","E","I","O","U"];
+ let count = 0;
+ str.split("").forEach((char) =>{
+    if (vowels.includes(char)){
+        count++;
+    }
+    return count;
+ })
+ }
+ var str = "Shaily";
+ console.log(countVowels(str));
+// 
+ 
    
+  
+
+
+
+
+
